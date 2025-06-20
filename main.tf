e036487fcb86ceb27db7bd8984717d93e8335e50
sekariyadeis <85100110+sekariyad2000@users.noreply.github.com>
2025-06-20 12:27:07 +0200

    Added syslog security rules

--- main.tf
+++ main.tf
@@ -86,6 +86,34 @@ resource "azurerm_network_security_rule" "ssh_sr" {
       network_security_group_name = azurerm_network_security_group.AKSsg.name
 }
 
+resource "azurerm_network_security_rule" "syslog_tcp" {
+  name                        = "syslog-tcp"
+  priority                    = 106
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "5514"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = data.azurerm_resource_group.main.name
+  network_security_group_name = azurerm_network_security_group.AKSsg.name
+}
+
+resource "azurerm_network_security_rule" "syslog_udp" {
+  name                        = "syslog-udp"
+  priority                    = 107
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Udp"
+  source_port_range           = "*"
+  destination_port_range      = "5514"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = data.azurerm_resource_group.main.name
+  network_security_group_name = azurerm_network_security_group.AKSsg.name
+}
+
 # Define Subnet for AKS
 resource "azurerm_subnet" "aks_subnet" {
   name                 = "aks-subnet"
